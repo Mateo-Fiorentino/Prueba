c1d1fa9bc1a6ee5a970b240b7413822652540f16
Mateo Fiorentino <fiorentinomateo309@gmail.com>
2026-04-01 02:13:52 -0300

    introduccion

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function MostrarMeses(){
+    const Meses = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","dieciembre"];
+    console.log(Meses);
+}
+
