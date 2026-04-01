69b98de3cf1a2ac4855ea8eeb410394b0e8622d4
Alumno <alumno@et26.local>
2026-04-01 15:15:27 -0300

    introduccion

--- js/index.js
+++ js/index.js
@@ -3,3 +3,49 @@ function MostrarMeses(){
     console.log(Meses);
 }
 
+var suma = 0, resta = 0, división = 0, multiplicación = 0, potencia = 0;
+
+function ejercicio2(){
+    var valores = [true, 5, false, "hola", "adios",2];
+    
+    suma = (valores[1] + valores[5])
+    resta = (valores[1] - valores[5])
+    división = (valores[1] / valores[5])
+    multiplicación = (valores[1] * valores[5])
+    potencia = (valores[1] ** valores[5])
+
+    console.log("el texto mayor es: ", valores[4]);
+    console.log("suma: ", suma);
+    console.log("resta: ", resta);
+    console.log("división: ", división);
+    console.log("multiplicación: ", multiplicación);
+    console.log("potencia: ", potencia);
+
+    console.log(valores);
+}
+
+var numero1 = 5;
+var numero2 = 8;
+if(numero1 < numero2) {
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2 > 0) {
+console.log("numero2 es positivo");
+}
+if(numero1 < 0 || numero1 !== 0) {
+console.log("numero1 es negativo o distinto de cero");
+}
+if((numero1 + 1) < numero2 || (numero1 + 1) !== numero2) {
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+
+function ejercicio4(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K','E', 'T'];
+
+    asdss
+
+}
+
+function ejercicio5(){
+    
+}
