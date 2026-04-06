98a9e040e199c01c881e70ae7b531b57d2a836a2
Alumno <alumno@et26.local>
2026-04-06 17:19:19 -0300

    introduccion ejer5-ejer8

--- js/index.js
+++ js/index.js
@@ -22,30 +22,130 @@ function ejercicio2(){
     console.log("potencia: ", potencia);
 
     console.log(valores);
-}
 
-var numero1 = 5;
-var numero2 = 8;
+}
+function ejercicio3(){
+
 if(numero1 < numero2) {
+
+    var numero1 = 5;
+    var numero2 = 8;
+
 console.log("numero1 no es mayor que numero2");
-}
-if(numero2 > 0) {
-console.log("numero2 es positivo");
-}
-if(numero1 < 0 || numero1 !== 0) {
-console.log("numero1 es negativo o distinto de cero");
-}
-if((numero1 + 1) < numero2 || (numero1 + 1) !== numero2) {
-console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+ }
+    if(numero2 > 0) {
+        console.log("numero2 es positivo");
+    }
+    if(numero1 < 0 || numero1 !== 0) 
+    {
+     console.log("numero1 es negativo o distinto de cero");
+    }
+    if((numero1 + 1) < numero2 || (numero1 + 1) !== numero2) 
+    {
+        console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+    }
 }
 
 function ejercicio4(){
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K','E', 'T'];
 
-    asdss
+    var dni = prompt("Escribe tu DNI: ")
 
+    if(dni == null || isNaN(dni) || dni == "" || dni < 0 || dni > 99999999)
+    {
+        console.log("El número proporcionado no es válido");
+    }
+
+    else
+    {
+        var resto = dni % 23;
+
+    console.log("Resto: ", resto); 
+    console.log("Letra: ", letras[resto]);
+    }
 }
 
 function ejercicio5(){
     
+    var resultado = 1;
+    var numero = prompt("ingrese un número ");
+
+    if(dni == null || isNaN(dni) || dni == "" || numero == 0){
+        console.log("el caracter ingresado no es valido");
+    }
+
+    else{
+
+        for(var i = 1; i <= numero; i++)
+        {
+            resultado = resultado * i;
+        }
+        console.log("El Factorial es: ", resultado)
+    }
+}
+
+function ejercicio6(){
+
+    var numero = prompt("ingrese un número:");
+     
+    if(numero == null || isNaN(numero) || numero == "" || numero == 0)
+    {
+        console.log("Lo ingresado no es valido");
+    }
+
+    else if(numero % 2 === 0){
+
+        document.writeln("el número es: ", numero);
+        document.writeln("el número es par");
+    }
+
+    else{
+        document.writeln("el numero es impar");
+    }
+}
+
+function ejercicio7(){
+
+var texto = prompt("ingrese una palabra:");
+
+     if(texto == null || !isNaN(texto) || texto == "" )
+    {
+        console.log("Lo ingresado no es valido");
+    }
+
+
+    if(texto === texto.toUpperCase()){
+        console.log("El texto esta formado por Mayúsculas")
+    }
+
+    else if(texto === texto.toLowerCase()){
+        console.log("El texto esta formado por Minúsculas")
+    }
+
+    else{
+        console.log("El texto esta formado por una mezcla de mayúsculas y minúsculas")
+    }
+}
+
+function ejercicio8(){
+    let texto = prompt("ingrese un texto:");
+    let str = texto.replaceAll(" "," ").toLocaleLowerCase();
+    let arrStr = str.split("").reverse().join("");
+
+    if (str == arrStr){
+        console.log("El texto ingresado es un palindromo");
+    }
+    else{
+        console.log("El texto ingresado no es un palindromo");
+    }
+}
+
+function ejercicio10(){
+    let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    for(i = 0; i < 36000; i++)
+    {
+        let primerdado = Math.floor(Math.random() * 6) + 1;
+        let segundodado = Math.floor(Math.random() * 6) + 1;
+    }
 }
